d846f5c92a3e80048dfdfaba76667c2395e07061
gregwinther <sebastwi@uio.no>
2016-10-23 22:48:14 +0200

    Removed old files

--- Project3/data/twosystem.m
+++
@@ -1,9 +0,0 @@
-load twosystem.dat;         %  read data 
-
-x_sun = twosystem(:,1);     
-y_sun = twosystem(:,2);   
-
-x_earth = twosystem(:,4);    
-y_earth = twosystem(:,5);    
-
-plot(x_earth,y_earth,x_sun,y_sun);
